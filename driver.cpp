19a91cc0ce7a97cd6c273546bdbee470ee335972
Austin Anderson <Austin.David.Anderson1@gmail.com>
2016-05-26 12:04:44 -0500

    added test for a class with overloaded << operator

--- driver.cpp
+++ driver.cpp
@@ -6,6 +6,20 @@ Created: Thu May 26 11:18:34 CDT 2016
 #include <vector>
 #include "vectorio.h"
 using namespace std;
+class Printable{
+    public:
+        Printable(int s1,double s2){
+            stuffOne=s1;
+            stuffTwo=s2;
+        }
+        friend ostream& operator<<(ostream& o,Printable& p){
+            o<<p.stuffOne<<", "<<p.stuffTwo;
+            return o;
+        }
+    private:
+        int stuffOne;
+        double stuffTwo;
+};
 int main(){
     vector<vector<int> > testLevelOne;
     int initOne[6]={1,2,3,4,5,6}; int* arr=initOne;
@@ -34,6 +48,10 @@ int main(){
     vector<double> testInputDouble;
     testInputDouble>>1.002>>2.10>>3.0>>4.8>>5.0;
     cout<<testInputDouble<<endl;
+
+    vector<Printable> printables;
+    printables>>Printable(1,2.4)>>Printable(3,2.001)>>Printable(1,7.232);
+    cout<<printables<<endl;
 
 
     return 0;
